aad794504d380701539e5ec34a359a82cdb4c92a
pmartin <contact@elol.fr>
2016-09-30 10:58:43 +0200

    Class added with 'ng generate myclass class1'

---
+++ src/app/class1.spec.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+
+import { addProviders, async, inject } from '@angular/core/testing';
+import {Class1} from './class1';
+
+describe('Class1', () => {
+  it('should create an instance', () => {
+    expect(new Class1()).toBeTruthy();
+  });
+});
